dc6734328d525fe8be9a041a268cc131d8f74468
Maithili <maithilishallu143@gmail.com>
2026-02-08 12:04:41 +0530

    Fix router placement to resolve React error #200

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import ForgotPassword from "../components/ForgotPassword";
 import ResetPassword from "../components/ResetPassword";
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/forgot-password" component={ForgotPassword} />
-        <Route exact path="/reset-password/:token" component={ResetPassword} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/forgot-password" component={ForgotPassword} />
+      <Route exact path="/reset-password/:token" component={ResetPassword} />
+    </Switch>
   );
 }
