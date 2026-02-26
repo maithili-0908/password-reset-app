ab0976571ad374d07ca4c8b43f2fe64f92f70593
Maithili <maithilishallu143@gmail.com>
2026-02-26 17:47:13 +0530

    Fix route undefined error and upgrade Node version to 18

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  createUser,
-  getUsers,
-  forgotPassword,
-  validateToken,
-  resetPassword
-} = require("../controllers/authController");
+const authController = require("../controllers/authController");
 
-// User management
-router.post("/users", createUser);
-router.get("/users", getUsers);
-
-// Password reset flow
-router.post("/forgot-password", forgotPassword);
-router.get("/reset-password/:token", validateToken);
-router.post("/reset-password/:token", resetPassword);
+// Password reset routes ONLY
+router.post("/forgot-password", authController.forgotPassword);
+router.get("/reset-password/:token", authController.validateToken);
+router.post("/reset-password/:token", authController.resetPassword);
 
 module.exports = router;
