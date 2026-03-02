7533e5c1639766e01bab256e808b056526616fba
Maithili <maithilishallu143@gmail.com>
2026-03-02 12:32:02 +0530

    changed mailjs

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -3,11 +3,14 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false,
+  secure: false, // IMPORTANT
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000, // 10 seconds
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
   tls: {
     rejectUnauthorized: false,
   },
