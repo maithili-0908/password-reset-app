865fc8dd8d6e55cd6a993f406eee2c4b6c100d77
Maithili <maithilishallu143@gmail.com>
2026-03-02 11:41:43 +0530

    changed authcontrol js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const crypto = require("crypto");
-const nodemailer = require("nodemailer");
+const transporter = require("../config/mail");
 
 
 // ================= REGISTER =================
