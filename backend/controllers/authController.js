e08f00b3c574510cc74fb91ee8b3cfc6450a5cd7
Maithili <maithilishallu143@gmail.com>
2026-03-03 14:16:15 +0530

    Fixed controller and routes separation

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,14 +1,46 @@
+const crypto = require("crypto");
 const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const sendEmail = require("../config/mail");
+
+// FORGOT PASSWORD
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const resetToken = crypto.randomBytes(32).toString("hex");
+
+    user.resetToken = resetToken;
+    user.resetTokenExpiry = Date.now() + 15 * 60 * 1000;
+    await user.save();
+
+    const resetLink = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+
+    await sendEmail(
+      user.email,
+      "Password Reset",
+      `<a href="${resetLink}">${resetLink}</a>`
+    );
+
+    res.json({ message: "Reset link sent" });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
 
 // RESET PASSWORD
 exports.resetPassword = async (req, res) => {
   try {
     const { token } = req.params;
     const { password } = req.body;
-
-    if (!password) {
-      return res.status(400).json({ message: "Password is required" });
-    }
 
     const user = await User.findOne({
       resetToken: token,
@@ -19,7 +51,6 @@ exports.resetPassword = async (req, res) => {
       return res.status(400).json({ message: "Invalid or expired token" });
     }
 
-    // Hash new password
     const hashedPassword = await bcrypt.hash(password, 10);
 
     user.password = hashedPassword;
@@ -31,7 +62,7 @@ exports.resetPassword = async (req, res) => {
     res.json({ message: "Password reset successful" });
 
   } catch (error) {
-    console.error("Reset Password Error:", error);
+    console.error(error);
     res.status(500).json({ message: "Server Error" });
   }
 };
